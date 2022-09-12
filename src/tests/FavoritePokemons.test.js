cb6518ad353b1ef989591599ea90a1c39864bdff
Vinicius Andrade <vini.fisica.unb@gmail.com>
2022-09-12 09:57:06 -0400

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testando a página Favorite Pokemons', () => {
+  test('Testando se a mensagem "No favorite pokemon found" aparece', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavorites = screen.getByText('No favorite pokemon found');
+    expect(noFavorites).toBeInTheDocument();
+  });
+});
