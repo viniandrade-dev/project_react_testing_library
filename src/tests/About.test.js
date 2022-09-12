847eea2ab0ca676aa58deda5f98f248fbe838fa6
Vinicius Andrade <vini.fisica.unb@gmail.com>
2022-09-12 09:47:50 -0400

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testando componente About', () => {
+  test('Testando se a página cotém um heading h2 com texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexHeading = screen
+      .getByRole('heading', { name: 'About Pokédex', level: 2 });
+
+    expect(pokedexHeading).toBeInTheDocument();
+    expect(pokedexHeading).toHaveTextContent('About Pokédex');
+  });
+
+  test('Testando se a página contém a imagem correta', () => {
+    renderWithRouter(<About />);
+    const pokedexImg = screen.getByRole('img');
+    const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg).toHaveAttribute('src', imgURL);
+  });
+});
